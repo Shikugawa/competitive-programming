8238346be032bdcdef3431e1e0ded35545ce2923
shikugawa <shikugawa@gmail.com>
2019-05-27 02:46:22 +0900

    add template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+using namespace std;
+
+using ll = long long int;
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec)
+{
+  for (int i = 0; i < vec.size(); i++)
+  {
+    os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+  }
+  return os;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+}
